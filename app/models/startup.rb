490fbddf1e58005873ba5df3bc8340fffc057ca8
esmasher <esmasherr@gmail.com>
2020-12-21 15:44:57 -0600

    commit tree 39f1528d7dca92151ec5f680b19d3cae0b01dfed

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,15 +1,1 @@
-class Startup
-    attr_accessor :name, :domain
-    attr_reader :founder
-    def initialize(name, founder, domain)
-        @name = name
-        @founder = founder
-        @domain = domain
-    end
 
-    def pivot(name, domain)
-        Start.new(name, domain)
-        binding.pry
-    end
-
-end
--- tools/console.rb
+++ tools/console.rb
@@ -10,5 +10,11 @@ end
 startup_one = Startup.new("Pizza", "Esma", "Food")
 startup_two = Startup.new("Pants", "Awais", "Clothes")
 startup_three = Startup.new("Box", "Esma", "Storage")
+
+vc_one = VentureCapitalist.new("Krane", 1000000000)
+vc_two = VentureCapitalist.new("Kane", 20000)
+vc_three = VentureCapitalist.new("Kne", 30000000)
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
